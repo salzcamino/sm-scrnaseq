75a083df334660352942844bcd2a0c8b7ad7dcdb
salamhha <salam.alabdullatif@gmail.com>
2025-11-17 01:02:28 +0300

    minor style updates

--- scripts/import-scanpy.py
+++ scripts/import-scanpy.py
@@ -4,7 +4,6 @@ import scanpy as sc
 adata = sc.read_10x_mtx(snakemake.input[0])
 sample = snakemake.wildcards.sample
 params = snakemake.config["samples"][sample]
-# Create AnnData object, remove features with counts in fewer than params["min_cells"] cells, remove cells with fewer than params["min_genes"] features
 adata.obs["sample"] = sample  # Add sample identity
 
 # Calculate base QC metrics
--- scripts/plot-qc-scanpy.py
+++ scripts/plot-qc-scanpy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import scanpy as sc
 import matplotlib.pyplot as plt
 
@@ -11,7 +11,7 @@ sc.pl.violin(adata, "n_genes_by_counts", jitter=0.4, ax=axes[0, 0], show=False)
 sc.pl.violin(adata, "total_counts", jitter=0.4, ax=axes[0, 1], show=False)
 sc.pl.violin(adata, "pct_counts_mt", jitter=0.4, ax=axes[0, 2], show=False)
 
-# Bottom row: scatter plots 
+# Bottom row: scatter plots
 sc.pl.scatter(adata, "total_counts", "n_genes_by_counts", 
               ax=axes[1, 0], show=False)
 sc.pl.scatter(adata, "total_counts", "pct_counts_mt", ax=axes[1, 1], show=False)
