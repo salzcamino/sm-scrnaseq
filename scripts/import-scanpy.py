ffb23c2a1acdbee36e638b0fc59765496df8000c
salamhha <salam@salz.localdomain>
2025-11-16 14:18:12 +0300

    update envs

--- scripts/import-scanpy.py
+++ scripts/import-scanpy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
 import scanpy as sc
 
-data = sc.read_10x_mtx(snakemake.input[0])
-data.write(snakemake.output[0])
+adata = sc.read_10x_mtx(snakemake.input[0])
+adata.write(snakemake.output[0])
