0d42228eb25625d53c7c54c998daa3f969bda2fd
salamhha <salam.alabdullatif@gmail.com>
2025-11-23 18:47:11 +0300

    Save copy of raw data

--- scripts/normalize-scanpy.py
+++ scripts/normalize-scanpy.py
@@ -2,7 +2,11 @@
 import scanpy as sc
 
 adata = sc.read_h5ad(snakemake.input[0])
+# Saving count data
+adata.layers["counts"] = adata.X.copy()
+# Normalizing to median total counts
 sc.pp.normalize_total(adata)
+# Logarithmize the data
 sc.pp.log1p(adata)
 
 adata.write(snakemake.output[0])
