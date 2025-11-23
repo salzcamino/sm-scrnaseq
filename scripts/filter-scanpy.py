ea5f41f25f9099d61bbdffdc97bf9b5f74f258b1
salamhha <salam.alabdullatif@gmail.com>
2025-11-23 18:40:17 +0300

    adjust filtering

--- scripts/filter-scanpy.py
+++ scripts/filter-scanpy.py
@@ -5,9 +5,13 @@ adata = sc.read_h5ad(snakemake.input[0])
 sample = snakemake.wildcards.sample
 params = snakemake.config["samples"][sample]
 
+# Filter cells with fewer than min_genes
 sc.pp.filter_cells(adata, min_genes=params["min_genes"])
+# Filter genes with fewer than min_cells
 sc.pp.filter_genes(adata, min_cells=params["min_cells"])
+# Filter cells with mitchondrial read % > max_mt_percent
 adata = adata[adata.obs['pct_counts_mt'] < params["max_mt_percent"], :]
+# Filter cells with gene count > max_genes
 adata = adata[(adata.obs['n_genes_by_counts'] < params["max_genes"]), :]
 
 adata.write(snakemake.output[0])
--- scripts/filter-seurat.R
+++ scripts/filter-seurat.R
@@ -5,6 +5,12 @@ data.seurat <- readRDS(snakemake@input[[1]])
 sample <- snakemake@wildcards[["sample"]]
 params <- snakemake@config[["samples"]][[sample]]
 
-# Filter object by feature count and mitochondrial read count
-data.seurat <- subset(data.seurat, subset = nFeature_RNA > params$min_genes & nFeature_RNA < params$max_genes & percent.mt < params$max_mt_percent)
+# Filter genes expressed in fewer than min_cells
+counts <- GetAssayData(data.seurat, slot = "counts")
+genes.keep <- rownames(counts)[rowSums(counts > 0) >= params$min_cells]
+data.seurat <- subset(data.seurat, features = genes.keep)
+
+# Filter cells by feature count and mitochondrial read count
+data.seurat <- subset(data.seurat, subset = nFeature_RNA >= params$min_genes & nFeature_RNA < params$max_genes & percent.mt < params$max_mt_percent)
+
 saveRDS(data.seurat, snakemake@output[[1]])
