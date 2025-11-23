2ddc510c9102f580efbafacfcc8aa3ba3808908b
salamhha <salam.alabdullatif@gmail.com>
2025-11-23 18:44:16 +0300

    typo fix

--- scripts/filter-scanpy.py
+++ scripts/filter-scanpy.py
@@ -9,7 +9,7 @@ params = snakemake.config["samples"][sample]
 sc.pp.filter_cells(adata, min_genes=params["min_genes"])
 # Filter genes with fewer than min_cells
 sc.pp.filter_genes(adata, min_cells=params["min_cells"])
-# Filter cells with mitchondrial read % > max_mt_percent
+# Filter cells with mitochondrial read % > max_mt_percent
 adata = adata[adata.obs['pct_counts_mt'] < params["max_mt_percent"], :]
 # Filter cells with gene count > max_genes
 adata = adata[(adata.obs['n_genes_by_counts'] < params["max_genes"]), :]
