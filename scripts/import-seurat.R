5f4ef6e840c5bcd969e465d1ed96ea193c76b16c
salamhha <salam.alabdullatif@gmail.com>
2025-11-23 18:52:51 +0300

    Add ribosomal and hemoglobin percent to Seurat import for consistency

--- scripts/import-seurat.R
+++ scripts/import-seurat.R
@@ -3,10 +3,12 @@ library(Seurat)
 # Read 10X data into counts matrix
 data.counts <- Read10X(data.dir = snakemake@input[[1]])
 sample <- snakemake@wildcards[["sample"]]
-params <- snakemake@config[["samples"]][[sample]]
+
 # Create Seurat object
 data.seurat <- CreateSeuratObject(counts = data.counts, project = sample)
-# Add mitochondrial read percentage
+# Add mitochondrial, ribosomal, and hemoglobin read percentage
 data.seurat[["percent.mt"]] <- PercentageFeatureSet(data.seurat, pattern = "^MT-")
+data.seurat[["percent.rb"]] <- PercentageFeatureSet(data.seurat, pattern = "^RPS|^RPL")
+data.seurat[["percent.hb"]] <- PercentageFeatureSet(data.seurat, pattern = "^HB[^(P)]")
 
 saveRDS(data.seurat, file = snakemake@output[[1]])
